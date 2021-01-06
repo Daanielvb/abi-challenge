8ae729e64fa05c787c0fd1a641b9abc6ffe2231b
Daniel Cirne <dcirne@squaretrade.com>
2021-01-06 10:27:29 -0800

    Initial structure

---
+++ src/main/java/com/ambev/abichallenge/AbiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.ambev.abichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ambev/abichallenge/AbiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambev.abichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
